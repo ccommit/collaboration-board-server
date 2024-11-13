8a07fe03116eeb511728d497c446f40a27e74696
kdong <kdong0712@naver.com>
2024-11-13 21:52:06 +0900

    issue1 프로젝트 생성 - springframework.boot' version '3.3.5' - spring-boot-starter-web - mysql - mybatis

---
+++ src/main/java/com/commit/collaboration_board_server/CollaborationBoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.commit.collaboration_board_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborationBoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborationBoardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commit/collaboration_board_server/CollaborationBoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commit.collaboration_board_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborationBoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
