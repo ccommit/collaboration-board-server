3d633c8b81867cbd830fd27a39d0c4716b285560
kdong <kdong0712@naver.com>
2024-12-04 22:10:35 +0900

    issue2 User Mybatis mysql 연동 - mybatis 버전 3.0.3 - test 코드 빌드시 오류때문 주석 - role, articles, calendar calss는 추후 생성예정

---
+++ src/main/java/com/commit/collaboration_board_server/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.commit.collaboration_board_server.service;
+
+import com.commit.collaboration_board_server.mapper.UserMapper;
+import com.commit.collaboration_board_server.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userMapper.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userMapper.findById(id);
+    }
+
+    @Override
+    public void createUser(User user) {
+        userMapper.insertUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userMapper.updateUser(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userMapper.deleteUser(id);
+    }
+}
